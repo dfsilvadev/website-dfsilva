c5bdc020f4034c450291776ad5f620c26b698ae8
Daniel Silva <dfsilva.dxp@gmail.com>
2024-03-06 19:32:39 -0300

    feat: add arquitetura inicial.

--- docs/pull_request_template.md
+++ docs/pull_request_template.md
@@ -6,7 +6,7 @@ Remova esse comentário e descreva aqui qual o objetivo desse PR.
 
 ### 🔗 Referências
 
-[FIGMA](https://www.figma.com/community/file/1138814493269096792)
+[FIGMA](https://www.figma.com/proto/cfKcBrLbPGQHZWvyR3OPBQ/Portf%C3%B3lio-v2?page-id=0%3A1&type=design&node-id=501-54&viewport=-1651%2C-990%2C0.98&t=lQS8YPniNfAWDjkO-1&scaling=scale-down&starting-point-node-id=501%3A54&mode=design)
 
 ### 📗 Prints
 
--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,34 +1,23 @@
-// eslint-disable-next-line no-undef
 module.exports = (plop) => {
-  plop.setGenerator("component", {
-    description: "Create a component",
+  plop.setGenerator('component', {
+    description: 'Create a component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "What is your component name?"
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
       }
     ],
     actions: [
       {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/index.tsx",
-        templateFile: "template/Component.tsx.hbs"
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'template/components/Component.tsx.hbs'
       },
       {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/styles.ts",
-        templateFile: "template/styles.ts.hbs"
-      },
-      {
-        type: "add",
-        path: "../src/stories/components/{{camelCase name}}.stories.tsx",
-        templateFile: "template/stories.tsx.hbs"
-      },
-      {
-        type: "add",
-        path: "../src/tests/components/{{camelCase name}}.test.tsx",
-        templateFile: "template/test.tsx.hbs"
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'template/components/styles.ts.hbs'
       }
     ]
   });
