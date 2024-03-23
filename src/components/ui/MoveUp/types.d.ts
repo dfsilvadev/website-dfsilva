1c30adaab3f79206d75678bee111cbd1f4d3dc5b
Daniel Silva <dfsilva.dxp@gmail.com>
2024-03-23 10:26:09 -0300

    nit: itens deletados.

--- src/components/ui/MoveUp/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { HTMLAttributes, ReactNode } from 'react';
-
-export type MoveUpInType = HTMLAttributes<HTMLSpanElement>;
-
-export interface IMoveUpInProps extends MoveUpInType {
-  children: ReactNode;
-}
